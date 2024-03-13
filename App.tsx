2aae0ae1a2b18d836846b74f56051047f3d820f0
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-13 10:28:53 +0700

    update routing

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/home/HomeScreen';
 import { useFonts } from 'expo-font';
+import Onboard1 from './screens/onboard1';
+import Onboard3 from './screens/onboard3';
+import Onboard2 from './screens/onboard2';
 
 
 
@@ -21,6 +24,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="onboard1" component={Onboard1} />
+        <Stack.Screen name="onboard2" component={Onboard2} />
+        <Stack.Screen name="onboard3" component={Onboard3} />
         <Stack.Screen name="home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
