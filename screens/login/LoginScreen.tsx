ebb94843999fd2e84e034eecdee4259369685d39
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-14 07:41:05 +0700

    update login screen page

--- screens/login/LoginScreen.tsx
+++ screens/login/LoginScreen.tsx
@@ -1,7 +1,8 @@
 import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons';
-import ButtonBack from '../../components/elements/buttonBack/ButtonBack';
+import ButtonBack from '../../components/elements/buttonBack';
+
 
 const LoginScreen = () => {
     return (
