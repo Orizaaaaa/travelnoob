08c9638be901f899eed2d2fed82a4cd3215d7420
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-14 09:17:28 +0700

    finish login page

--- utils/dataObject.tsx
+++ utils/dataObject.tsx
@@ -2,17 +2,17 @@
 export const connection = [
     {
         icon: require('../assets/images/ig.png'),
-        link: 'sms:081234567890?body=Halo'
+        link: 'sms:081234567890?body=Belum di aktifkan hhe'
 
     },
     {
         icon: require('../assets/images/fb.png'),
-        link: 'sms:081234567890?body=Halo'
+        link: 'sms:081234567890?body=Belum di aktifkan hhe'
 
     },
     {
         icon: require('../assets/images/github.png'),
-        link: 'sms:081234567890?body=Halo'
+        link: 'sms:081234567890?body=Belum di aktifkan hhe'
 
     }
 ]
