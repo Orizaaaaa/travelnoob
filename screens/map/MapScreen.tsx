bf99525fd6278b0625e845d6f08e2cc6964f2fce
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-20 11:28:06 +0700

    finish create map screen

--- screens/map/MapScreen.tsx
+++ screens/map/MapScreen.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import ButtonPrimary from '../../components/elements/buttonPrimary';
+import ButtonSecondary from '../../components/elements/ButtonSecondary';
 
 
 
@@ -14,24 +18,42 @@ export default function MapScreen() {
     };
 
     return (
-        <View style={styles.container}>
-            <MapView style={styles.map} initialRegion={initialRegion}>
+        <View className='flex-1 relative'>
+            <View className='absolute w-full bottom-0 p-4' >
+                <View className='flex h-48 bg-gray-600 opacity-80 p-4 rounded-xl  z-10' >
+                    <View className='flex-row justify-between' >
+                        <Text className='text-white text-lg' >Niladri Reservoir</Text>
+                        <View className='flex-row items-center space-x-1' >
+                            <FontAwesome name="star" size={19} color="#FFD336" />
+                            <Text className='text-lg text-white' >4.5</Text>
+                        </View>
+                    </View>
+
+                    <View className='flex-row items-center space-x-1 mt-2' >
+                        <Ionicons name="location-outline" size={17} color="#fff" />
+                        <Text className='text-white font-Inter' >Tekergat, Sunamgnj</Text>
+                    </View>
+                    <View className='flex-row items-center space-x-2 mt-2' >
+                        <MaterialCommunityIcons name="clock-outline" size={17} color="#fff" />
+                        <Text className='text-white font-Inter' >45 Minutes</Text>
+                    </View>
+
+                    <ButtonPrimary className='w-full h-12 rounded-xl mt-4'  >
+                        <Text className='text-lg font-medium text-white '>
+                            Booking Now
+                        </Text>
+                    </ButtonPrimary>
+                </View>
+            </View>
+
+            <MapView className='w-full h-full' initialRegion={initialRegion}>
                 <Marker
                     coordinate={{ latitude: initialRegion.latitude, longitude: initialRegion.longitude }}
                     title={"Pusat Peta"}
                     description={"Ini adalah lokasi pusat peta"}
                 />
-            </MapView>
-        </View>
+            </MapView >
+
+        </View >
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    map: {
-        width: '100%',
-        height: '100%',
-    },
-});
